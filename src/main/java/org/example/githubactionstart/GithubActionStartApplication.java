ed7ec08d6e12748542b8840326ad0af19b55506b
johan-Andersen <joan0005@stud.kea.dk>
2025-09-04 16:38:41 +0200

    Initial commit

---
+++ src/main/java/org/example/githubactionstart/GithubActionStartApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubactionstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionStartApplication.class, args);
+    }
+
+}
